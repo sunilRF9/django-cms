6fb83e2b5648a3cb995731f5039e1e5687c2bde3
sunil <sunil1996@gmail.com>
2020-06-12 14:17:05 +0530

    database config

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+#coutinhod3dx9
 from django.db import models
-
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+    email = models.EmailField(max_length=254,null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return "Name: {}, Email: {} and Phone: {}".format(self.name,self.email,self.phone)

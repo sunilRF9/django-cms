cdb5d2850d5c6b1dd8d7887a15a5fdec326e1a32
sunil <sunil1996@gmail.com>
2020-06-12 20:39:35 +0530

    database designed

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,3 @@
-#coutinhod3dx9
 from django.db import models
 # Create your models here.
 class Customer(models.Model):
@@ -9,3 +8,36 @@ class Customer(models.Model):
 
     def __str__(self):
         return "Name: {}, Email: {} and Phone: {}".format(self.name,self.email,self.phone)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY= (
+            ('Indoor','Indoor'),
+            ('Outdoor', 'Outdoor'),
+            )
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200,null=True,choices=CATEGORY)
+    description = models.CharField(max_length=200,null=True,blank=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+    tags = models.ManyToManyField(Tag)
+    def __str__(self):
+        return "Name: {} and Price: {}".format(self.name,self.price)
+
+class Order(models.Model):
+    STATUS = (
+            ('Pending','Pending'),
+            ('Out for delivery', 'Out for delivery'),
+            ('Delivered', 'Delivered'),
+            )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True,null=True) 
+    status = models.CharField(max_length=200,null=True,choices=STATUS)
+    def __str__(self):
+        return "Status: {}".format(self.status)
